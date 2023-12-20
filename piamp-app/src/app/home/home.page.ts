0b273fa174a25298d5cad10b50b1f7361522b704
Thomas Sparber <thomas@sparber.eu>
2023-12-20 09:11:24 +0100

    Added parameter sliders to app

--- piamp-app/src/app/home/home.page.ts
+++ piamp-app/src/app/home/home.page.ts
@@ -1,11 +1,17 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { Platform } from '@ionic/angular';
-import { BleClient, BleDevice } from '@capacitor-community/bluetooth-le';
+import { BleClient, BleDevice, numberToUUID } from '@capacitor-community/bluetooth-le';
 
 interface UIRequest {
   method?: "GET" | "POST";
   url: string;
   payload?: any;
+}
+
+interface Parameter {
+  label: string;
+  value: number;
+  characteristics: number;
 }
 
 @Component({
@@ -15,19 +21,55 @@ interface UIRequest {
 })
 export class HomePage {
   
-  @ViewChild("iframeObj", {static: false}) iframeObj?: ElementRef<HTMLIFrameElement>;
   connectedDevice?: BleDevice;
   serviceUUID = "22222222-3333-4444-5555-666666666666";
-  characteristicsUUID = "22222222-3333-4444-5555-666666666668";
   encoder = new TextEncoder();
   decoder = new TextDecoder();
+  parameters: Parameter[] = [{
+    label: "Gain",
+    value: 0,
+    characteristics: 0x0000,
+  }, {
+    label: "Master",
+    value: 0,
+    characteristics: 0x0001
+  }, {
+    label: "Bass",
+    value: 0,
+    characteristics: 0x0002
+  }, {
+    label: "Mid",
+    value: 0,
+    characteristics: 0x0003
+  }, {
+    label: "Treble",
+    value: 0,
+    characteristics: 0x0004
+  }, {
+    label: "Presence",
+    value: 0,
+    characteristics: 0x0005
+  }, {
+    label: "Model",
+    value: 0,
+    characteristics: 0x0006
+  }, {
+    label: "Ir",
+    value: 0,
+    characteristics: 0x0007
+  }, {
+    label: "Reverb",
+    value: 0,
+    characteristics: 0x0008
+  }, {
+    label: "Delay",
+    value: 0,
+    characteristics: 0x0009
+  }];
 
   constructor(
     private platform: Platform
   ) {
-    platform.ready().then(() => {
-      console.log(this.iframeObj);
-    });
   }
 
   async scanBLE() {
@@ -59,22 +101,19 @@ export class HomePage {
 
     console.log("Connected");
     this.connectedDevice = device;
-
-    this.loadUI();
   }
 
-  async loadUI() {
-    const index = await this.bleUIRequest({ url: "http://localhost:3600/index.html" });
-    this.iframeObj?.nativeElement.contentWindow?.document.open();
-    this.iframeObj?.nativeElement.contentWindow?.document.write(index);
-    this.iframeObj?.nativeElement.contentWindow?.document.close();
+  onValueChanged(ev: Event, parameter: Parameter) {
+    const stringValue = "" + (parameter.value / 100);
+    const dv = new DataView(this.encoder.encode(stringValue).buffer);
+    BleClient.write(this.connectedDevice!.deviceId, this.serviceUUID, numberToUUID(parameter.characteristics), dv);
   }
 
   async bleUIRequest(r: UIRequest) {
     const dv = new DataView(this.encoder.encode("uirequest " + JSON.stringify(r)).buffer);
-    await BleClient.write(this.connectedDevice!.deviceId, this.serviceUUID, this.characteristicsUUID, dv);
-    const response = await BleClient.read(this.connectedDevice!.deviceId, this.serviceUUID, this.characteristicsUUID);
-    return this.decoder.decode(response);
+    //await BleClient.write(this.connectedDevice!.deviceId, this.serviceUUID, this.characteristicsUUID, dv);
+    //const response = await BleClient.read(this.connectedDevice!.deviceId, this.serviceUUID, this.characteristicsUUID);
+    //return this.decoder.decode(response);
   }
 
 }
